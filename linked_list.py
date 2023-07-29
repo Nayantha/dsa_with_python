6a4b65efcd27a1277848bad2d43f7b3756c5ec0a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 19:40:42 +0530

    add insert at rear method to the linked list class.

--- linked_list.py
+++ linked_list.py
@@ -12,6 +12,13 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def insert_at_rear(self, data):
+        current_node: Node = self.head
+        while current_node.next_node:
+            current_node = current_node.next_node
+        new_node: Node = Node(data)
+        current_node.next_node = new_node
+
     def print(self):
         current_node: Node = self.head
         while current_node.next_node:
