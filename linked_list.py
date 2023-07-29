3a274eab799ad90215d29e012d992c7dd2962be4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 19:34:33 +0530

    add linked list class value print method.

--- linked_list.py
+++ linked_list.py
@@ -11,3 +11,10 @@ class LinkedList:
         new_node: Node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def print(self):
+        current_node: Node = self.head
+        while current_node:
+            print(current_node.data, sep=" -> ")
+            current_node = current_node.next_node
+        print("None")
