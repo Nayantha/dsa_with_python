83805debc75434f21c2b7c3d28564b8d46d088c1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 17:49:41 +0530

    add sample c code from previous example.

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,10 @@
+# int arr[] = {20,10,5,35,45,6,12,15,25,50,7};
+#     int n = sizeof(arr)/ sizeof(arr[0]), key;
+#     for (int i = 1; i < n; ++i) {
+#         key = arr[i];
+#         int j = i - 1;
+#         for (; j >= 0 && key < arr[j]; --j) {
+#             arr[j+1] = arr[j];
+#         }
+#         arr[j+1] = key;
+#     }
