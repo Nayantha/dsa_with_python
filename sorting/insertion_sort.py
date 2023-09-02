283b6ed0d99f9b716a5e950bfb63e6b9b8bc0fa7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 18:11:41 +0530

    extract sorting algorithm to a separate function.

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -10,12 +10,15 @@
 #     }
 array = [20, 10, 5, 35, 45, 6, 12, 15, 25, 50, 7]
 unsorted_array = array
-for index in range(1, len(array)):
-    number = array[index]
-    search_index = index - 1
-    while search_index >= 0 and number < array[search_index]:
-        search_index -= 1
-    array[index] = array[search_index + 1]
-    array[search_index + 1] = number
 unsorted_array.sort()
 assert array == unsorted_array
+
+
+def insertion_sort(array: list[int]):
+    for index in range(1, len(array)):
+        number = array[index]
+        search_index = index - 1
+        while search_index >= 0 and number < array[search_index]:
+            search_index -= 1
+        array[index] = array[search_index + 1]
+        array[search_index + 1] = number
