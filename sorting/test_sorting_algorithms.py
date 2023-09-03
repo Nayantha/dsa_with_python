e19d839028a49331799c65fa7f9619521fbe9c6b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 13:26:02 +0530

    rename the function to bypass test it.

--- sorting/test_sorting_algorithms.py
+++ sorting/test_sorting_algorithms.py
@@ -1,7 +1,7 @@
 from sorting.insertion_sort import insertion_sort
 
 
-def test_sorting_algorithm(sorting_algorithm):
+def sorting_algorithm_test(sorting_algorithm):
     array = [20, 10, 5, 35, 45, 6, 12, 15, 25, 50, 7]
     unsorted_array = array
     sorting_algorithm(array)
@@ -10,4 +10,4 @@ def test_sorting_algorithm(sorting_algorithm):
 
 
 def test_insertion_sort():
-    test_sorting_algorithm(insertion_sort)
+    sorting_algorithm_test(insertion_sort)
