8b9c38e66393fa7c76160dcb7410f11ea45df98a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 13:15:28 +0530

    extract the common testing to one function.

--- sorting/test_sorting_algorithms.py
+++ sorting/test_sorting_algorithms.py
@@ -1,9 +1,13 @@
 from sorting.insertion_sort import insertion_sort
 
 
-def test_insertion_sort():
+def test_sorting_algorithm(sorting_algorithm):
     array = [20, 10, 5, 35, 45, 6, 12, 15, 25, 50, 7]
     unsorted_array = array
-    insertion_sort(array)
+    sorting_algorithm(array)
     unsorted_array.sort()
     assert array == unsorted_array
+
+
+def test_insertion_sort():
+    test_sorting_algorithm(insertion_sort)
