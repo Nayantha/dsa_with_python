8d7e3f478532ff4c80d433ca9866d02f68170c51
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 14:20:52 +0530

    optimize the algorithm.

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,7 +1,5 @@
 def selection_sort(arr: list[int]):
     for i in range(len(arr)):
-        current_min_value = arr[i]
         for j in range(i + 1, len(arr)):
-            if current_min_value > arr[j]:
-                current_min_value = arr[j]
+            if arr[i] > arr[j]:
                 arr[j], arr[i] = arr[i], arr[j]
