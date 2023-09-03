50a18e63f177accf3230b720c8fa92bf8c049bf6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 13:45:23 +0530

    update the minimum value when looping in the inner loop.

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -2,4 +2,5 @@ def selection_sort(arr: list[int]):
     for i in range(len(arr)):
         current_min_value = arr[i]
         for j in range(i + 1, len(arr)):
-            ...
+            if current_min_value > arr[j]:
+                current_min_value = arr[j]
