83e749e2c3d99a88646020b3c1d4ce1c7b58fdfa
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-13 12:45:45 +0530

    add the logic to partition the list.

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,4 +1,18 @@
 def partition(arr: list[int], left_pointer: int, right_pointer: int):
-    ...
+    pivot_element = arr[right_pointer]
+    pivot_element_index = right_pointer
+    while True:
+        while pivot_element >= arr[left_pointer] and left_pointer < right_pointer:
+            left_pointer += 1
+        if left_pointer >= right_pointer:
+            break
+        while pivot_element < arr[right_pointer] and left_pointer < right_pointer:
+            right_pointer -= 1
+        if left_pointer >= right_pointer:
+            break
+        arr[left_pointer], arr[right_pointer] = arr[right_pointer], arr[left_pointer]
+    arr[pivot_element_index], arr[left_pointer] = arr[left_pointer],arr[pivot_element_index]
+    return left_pointer
+
 def quick_sort(array: list[int]):
     ...
