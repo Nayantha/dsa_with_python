3f5af030ab124824e60e5ffac3ecc9eb2ce80564
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-13 12:55:46 +0530

    add if name == main call.

--- sorting/test_sorting_algorithms.py
+++ sorting/test_sorting_algorithms.py
@@ -2,6 +2,7 @@ from insertion_sort import insertion_sort
 from selection_sort import selection_sort
 from quick_sort import quick_sort
 
+
 def sorting_algorithm_test(sorting_algorithm):
     array = [20, 10, 5, 35, 45, 6, 12, 15, 25, 50, 7]
     unsorted_array = array
@@ -17,9 +18,14 @@ def test_insertion_sort():
 def test_selection_sort():
     sorting_algorithm_test(selection_sort)
 
+
 def test_quick_sort():
     array = [20, 10, 5, 35, 45, 6, 12, 15, 25, 50, 7]
     unsorted_array = array
     quick_sort(array)
     unsorted_array.sort()
     assert array == unsorted_array
+
+
+if __name__ == "__main__":
+    test_quick_sort()
